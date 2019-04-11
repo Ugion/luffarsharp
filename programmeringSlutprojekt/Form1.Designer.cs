2494e67f0200a1fbf7679ac17fac9833e671ffe6
Alexandra Koch <alexandrakoch1@protonmail.com>
2019-04-11 12:58:35 +0200

    Basic board handling logic

---
+++ programmeringSlutprojekt/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace programmeringSlutprojekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box0 = new System.Windows.Forms.Button();
+            this.box8 = new System.Windows.Forms.Button();
+            this.box7 = new System.Windows.Forms.Button();
+            this.box6 = new System.Windows.Forms.Button();
+            this.box5 = new System.Windows.Forms.Button();
+            this.box4 = new System.Windows.Forms.Button();
+            this.box3 = new System.Windows.Forms.Button();
+            this.box2 = new System.Windows.Forms.Button();
+            this.box1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // box0
+            // 
+            this.box0.Location = new System.Drawing.Point(12, 13);
+            this.box0.Name = "box0";
+            this.box0.Size = new System.Drawing.Size(100, 100);
+            this.box0.TabIndex = 0;
+            this.box0.UseVisualStyleBackColor = true;
+            this.box0.Click += new System.EventHandler(this.btnClick);
+            // 
+            // box8
+            // 
+            this.box8.Location = new System.Drawing.Point(225, 225);
+            this.box8.Name = "box8";
+            this.box8.Size = new System.Drawing.Size(100, 100);
+            this.box8.TabIndex = 3;
+            this.box8.UseVisualStyleBackColor = true;
+            this.box8.Click += new System.EventHandler(this.btnClick);
+            // 
+            // box7
+            // 
+            this.box7.Location = new System.Drawing.Point(119, 225);
+            this.box7.Name = "box7";
+            this.box7.Size = new System.Drawing.Size(100, 100);
+            this.box7.TabIndex = 4;
+            this.box7.UseVisualStyleBackColor = true;
+            this.box7.Click += new System.EventHandler(this.btnClick);
+            // 
+            // box6
+            // 
+            this.box6.Location = new System.Drawing.Point(12, 225);
+            this.box6.Name = "box6";
+            this.box6.Size = new System.Drawing.Size(100, 100);
+            this.box6.TabIndex = 5;
+            this.box6.UseVisualStyleBackColor = true;
+            this.box6.Click += new System.EventHandler(this.btnClick);
+            // 
+            // box5
+            // 
+            this.box5.Location = new System.Drawing.Point(225, 119);
+            this.box5.Name = "box5";
+            this.box5.Size = new System.Drawing.Size(100, 100);
+            this.box5.TabIndex = 6;
+            this.box5.UseVisualStyleBackColor = true;
+            this.box5.Click += new System.EventHandler(this.btnClick);
+            // 
+            // box4
+            // 
+            this.box4.Location = new System.Drawing.Point(119, 119);
+            this.box4.Name = "box4";
+            this.box4.Size = new System.Drawing.Size(100, 100);
+            this.box4.TabIndex = 7;
+            this.box4.UseVisualStyleBackColor = true;
+            this.box4.Click += new System.EventHandler(this.btnClick);
+            // 
+            // box3
+            // 
+            this.box3.Location = new System.Drawing.Point(12, 119);
+            this.box3.Name = "box3";
+            this.box3.Size = new System.Drawing.Size(100, 100);
+            this.box3.TabIndex = 8;
+            this.box3.UseVisualStyleBackColor = true;
+            this.box3.Click += new System.EventHandler(this.btnClick);
+            // 
+            // box2
+            // 
+            this.box2.Location = new System.Drawing.Point(225, 13);
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(100, 100);
+            this.box2.TabIndex = 9;
+            this.box2.UseVisualStyleBackColor = true;
+            this.box2.Click += new System.EventHandler(this.btnClick);
+            // 
+            // box1
+            // 
+            this.box1.Location = new System.Drawing.Point(119, 13);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(100, 100);
+            this.box1.TabIndex = 10;
+            this.box1.UseVisualStyleBackColor = true;
+            this.box1.Click += new System.EventHandler(this.btnClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.box1);
+            this.Controls.Add(this.box2);
+            this.Controls.Add(this.box3);
+            this.Controls.Add(this.box4);
+            this.Controls.Add(this.box5);
+            this.Controls.Add(this.box6);
+            this.Controls.Add(this.box7);
+            this.Controls.Add(this.box8);
+            this.Controls.Add(this.box0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button box0;
+        private System.Windows.Forms.Button box8;
+        private System.Windows.Forms.Button box7;
+        private System.Windows.Forms.Button box6;
+        private System.Windows.Forms.Button box5;
+        private System.Windows.Forms.Button box4;
+        private System.Windows.Forms.Button box3;
+        private System.Windows.Forms.Button box2;
+        private System.Windows.Forms.Button box1;
+    }
+}
+
---
+++ programmeringSlutprojekt/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace programmeringSlutprojekt
+{
+    public partial class Form1 : Form
+    {
+        int?[] board = new int?[9];
+        int currPlayer = 1;
+        Color Red = Color.FromName("Red");
+        Color Green = Color.FromName("Green");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+        private bool registerClick(Button btn, int player)
+        {
+            //hämta den fjärde karaktären i knappens namn, alltså platsen i fältet
+            int btnNr = btn.Name[3] - 48;
+            if (board[btnNr] == null)
+            {
+                //ändra färg och notifiera eventhanteraren att en tillåten tryckning skett
+                board[btnNr] = player;
+                if (player == 1)
+                {
+                    btn.BackColor = Green;
+                }
+                else
+                {
+                    btn.BackColor = Red;
+                }
+                return true;
+            }
+            //returnera false, nuvarande spelares omgång fortsätter tills en tillåten tryckning skett
+            else
+            {
+                return false;
+            }
+        }
+        //händelselyssnare för gränssnittet
+        //avslut på händelselyssnare för gränssnittet
+        private void btnClick(object sender, EventArgs e)
+        {
+            Button Button = (Button)sender;
+            bool legalClick = registerClick(Button, currPlayer);
+            //om knapptryckningen var tillåten, fortsätt till nästa spelare
+            if (legalClick == true)
+            {
+                if (currPlayer == 1)
+                {
+
+                    currPlayer--;
+                    //kör datorns omgång
+                }
+                else
+                {
+                    //ge kontroll till spelaren
+                    currPlayer++;
+                }
+            }
+        }
+    }
+}
